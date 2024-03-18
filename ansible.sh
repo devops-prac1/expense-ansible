d5281ba193176ab47d3b1dac23d25a26b91f2c8b
dspriyasg <priya9.sdr@gmail.com>
2024-03-18 09:39:31 +0800

    gocd fronted

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
   component=$1
   env
-  ansible-playbook -i $component-dev.priya9sdr.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 env=dev -e role_name=$component expense.yml
+  #ansible-playbook -i $component-dev.priya9sdr.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 env=dev -e role_name=$component expense.yml
 
 
