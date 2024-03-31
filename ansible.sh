55e8deadafa7a777ff910eeeaec4b10f734aa1bc
dspriyasg <priya9.sdr@gmail.com>
2024-03-31 23:34:41 +0800

    adding  mysql dependancy to backend to run

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
   component=$1
 
-  ansible-playbook -i $component-$env.priya9sdr.online,  expense.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component
+  ansible-playbook -i $component-$env.priya9sdr.online,   -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
 
 
