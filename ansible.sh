effa0120c2e8498c9c8a44f2634d2e39688e3163
dspriyasg <priya9.sdr@gmail.com>
2024-04-12 17:49:27 +0800

    updated with app and secrets .json files

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
   component=$1
   ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
   ansible-playbook -i $component-$env.priya9sdr.online,   -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+  rm -f ~/secrets.json ~/app.json
 
 
