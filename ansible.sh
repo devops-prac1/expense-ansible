baca74445cb4d8b6dd6a72cc4265cae37a4a86d7
dspriyasg <priya9.sdr@gmail.com>
2024-04-15 15:44:48 +0800

    enable logstash with automation on component frontend

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
   component=$1
   rm -f ~/secrets.json ~/app.json
   ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-  ansible-playbook -i $component-$env.priya9sdr.online,   -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+  ansible-playbook -i $component-$env.priya9sdr.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
   rm -f ~/secrets.json ~/app.json
 
 
